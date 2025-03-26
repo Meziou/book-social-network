49f536a662dd5e2d56d5693bb29b7e9086c6dfec
chemsmeziou <chems.meziou@live.fr>
2025-03-26 07:15:17 +0100

    refactor: Update User entity to include roles and implement UserDetails

--- book-network/src/main/java/com/chems/book/user/User.java
+++ book-network/src/main/java/com/chems/book/user/User.java
@@ -5,17 +5,23 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import com.chems.book.role.Role;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Data;
@@ -39,7 +45,8 @@ public class User implements UserDetails, Principal{
     private boolean accountLocked;
     private boolean enabled;
 
-    // private List<Role> roles;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Role> roles;
     @CreatedDate
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdDate;
@@ -54,8 +61,10 @@ public class User implements UserDetails, Principal{
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
+        return this.roles
+                    .stream()
+                    .map(r -> new SimpleGrantedAuthority(r.getName()))
+                    .collect(Collectors.toList());
     }
 
     @Override
