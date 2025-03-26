a02efd023a8d9fc16727600a48611193c5cba70f
chemsmeziou <chems.meziou@live.fr>
2025-03-26 07:14:59 +0100

    chore: Add Role entity with JPA annotations and relationships

---
+++ book-network/src/main/java/com/chems/book/role/Role.java
@@ -0,0 +1,43 @@
+package com.chems.book.role;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.chems.book.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime lastModifiedDate;
+}
