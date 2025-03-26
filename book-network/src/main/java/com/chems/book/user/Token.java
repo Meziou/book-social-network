9cd5ea2cb240f97d6622fae96d3ccc089172ef05
chemsmeziou <chems.meziou@live.fr>
2025-03-26 07:24:33 +0100

    chore: Add Token entity and TokenRepository interface for token management

--- book-network/src/main/java/com/chems/book/user/Token.java
+++ book-network/src/main/java/com/chems/book/user/Token.java
@@ -1,6 +1,12 @@
 package com.chems.book.user;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import lombok.*;
 
 
@@ -10,5 +16,16 @@ import lombok.*;
 @Builder
 @Entity
 public class Token {
-	
+	@Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+    private  LocalDateTime creatdeAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToMany
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
 }
