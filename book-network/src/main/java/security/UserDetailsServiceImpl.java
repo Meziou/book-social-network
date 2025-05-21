dbad84cc70998c1854e0a4d8ea2804b201210a95
chemsmeziou <chems.meziou@live.fr>
2025-05-21 22:19:52 +0200

    feat: Create UserDetailsServiceImpl for custom user details retrieval

---
+++ book-network/src/main/java/security/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.chems.book.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + userEmail));
+    }
+}
