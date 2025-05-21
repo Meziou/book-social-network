4b9d6312229f6087321455f9000346af4ea721d8
chemsmeziou <chems.meziou@live.fr>
2025-05-21 22:02:46 +0200

    feat: Implement JWT authentication in SecurityConfig

---
+++ book-network/src/main/java/security/SecurityConfig.java
@@ -0,0 +1,49 @@
+package security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
+
+    private JwtFilter jwtauthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequest(req ->
+                    req.requestMatchers(
+                        "/api/auth/**",
+                        "/V2/api-docs",
+                        "/v3/api-docs",
+                        "/v3/api-docs/**",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/configuration/ui",
+                        "/configuration/security",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html",
+                        "/webjars/**"
+                    ).permiAll()
+                            .anyRequest()
+                                .authenticated()
+                    )
+                    .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                    .authenticationProvider(authenticationProvider)
+                    .addFilterBefore(jwtauthFilter, UsernamePasswordAuthenticationFilter.class);
+        
+    }
+}
